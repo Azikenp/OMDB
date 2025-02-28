d97519e4fec0f662f0390d739079972f2e386e4f
Azikenp <azikenp26@gmail.com>
2025-02-28 07:01:48 +0100

    changes to apiservice

--- src/services/OmdbApi.ts
+++ src/services/OmdbApi.ts
@@ -1,20 +1,23 @@
 import axios from "axios";
 
-const movieNames = ["Barbie", "Batman", "Red one"];
+const movieNames = ["barbie", "batman", "redone"];
 
 const getRandomMovie = (namesArray: string[]): string => {
   return namesArray[Math.floor(Math.random() * namesArray.length)];
 };
 
+const getRandomMovieValue = getRandomMovie(movieNames);
+
 const api_key = import.meta.env.VITE_API_KEY;
 
 const omdbURL = `https://www.omdbapi.com/?apikey=${api_key}&`;
 
-const getPoster = axios.get(omdbURL + getRandomMovie(movieNames));
+const getPoster = axios.get(omdbURL + getRandomMovieValue);
 
 const getMovieBySearch = (param: string) => axios.get(omdbURL + param);
 
 export default {
   getPoster,
   getMovieBySearch,
+  getRandomMovieValue,
 };
